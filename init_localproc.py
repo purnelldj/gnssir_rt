9f485126c168ed3963e2722499e38d89b2c8e815
purnelldj <31864158+purnelldj@users.noreply.github.com>
2023-12-19 12:37:11 -0500

    improved elv interp (#3)

--- init_localproc.py
+++ init_localproc.py
@@ -4,6 +4,5 @@ from pathlib import Path
 # make a directory called 'localproc'
 Path("localproc").mkdir(parents=True, exist_ok=True)
 
-# now copy data from testdata to localproc
-shutil.copytree("tests/testdata/sjdlr", "localproc/sjdlr", dirs_exist_ok=True)
-# shutil.copytree("tests/testdata/rv3s", "localproc/rv3s", dirs_exist_ok=True)
+# unzip sjdlr.zip to localproc
+shutil.unpack_archive("tests/testdata/sjdlr.zip", "localproc/sjdlr", "zip")
